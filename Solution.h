26361f72b7b4a1e3ea1c5a4cf379e8d3bc5b0d32
LeoBrilliant <LeoBrilliant@gmail.com>
2016-06-20 20:14:11 +0800

    twoSum solution 2

--- Solution.h
+++ Solution.h
@@ -20,18 +20,20 @@ class Solution
 public:
 	vector<int> twoSum(vector<int>& nums, int target)
 	{
+		map<int, int> buffer;
 		vector<int> result;
-		int index1, index2;
 		for(int i = 0; i < nums.size(); ++i)
 		{
-			for(int j = i + 1; j < nums.size(); ++ j)
+			buffer[nums[i]] = i;
+		}
+
+		for(int i = 0; i < nums.size(); ++i)
+		{
+			if(buffer.find(target - nums[i]) != buffer.end() && buffer[target - nums[i]] != i)
 			{
-				if(nums[i] + nums[j] == target)
-				{
-					result.push_back(i);
-					result.push_back(j);
-					return result;
-				}
+				result.push_back(i);
+				result.push_back(buffer[target - nums[i]]);
+				break;
 			}
 		}
 		return result;
@@ -39,16 +41,23 @@ public:
 
 	void twoSumTest()
 	{
-		int array[] = {2, 7, 11, 15};
-
-		vector<int>  nums(array);
-
-		int target = 9;
+		//vector<int>  nums {2, 7, 11, 15};
+		vector<int> nums {3,2,4};
+		int target = 6;
 
 		vector<int> result = this->twoSum(nums, target);
 
-		assert(result[0] == 0);
-		assert(result[1] == 1);
+		for(auto & p : result)
+			cout << p << ", ";
+		cout << endl;
+
+		assert(result.size() == 2);
+		assert(result[0] == 1);
+		assert(result[1] == 2);
+
+
+
+		std::cout << "Hello" << std::endl;
 	}
 };
 
