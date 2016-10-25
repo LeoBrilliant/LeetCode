6b91dc5bb7e619fffe01f764c605dfc562538687
LeoBrilliant <LeoBrilliant@gmail.com>
2016-10-25 10:17:00 +0800

    调整目录结构，前50道题里程碑

---
+++ P1~50/Common.h
@@ -0,0 +1,15 @@
+/*
+ * Common.h
+ *
+ *  Created on: 2016年10月25日
+ *      Author: user
+ */
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+
+#include "..\Common.h"
+
+
+#endif /* COMMON_H_ */
--- main.cpp
+++ main.cpp
@@ -5,117 +5,12 @@
  *      Author: LeoBrilliant
  */
 
-#include <iostream>
+#include "Common.h"
 
-#include "ThreeSum.h"
-
-#include "P11.h"
-#include "P12.h"
-#include "P14.h"
-#include "P377.h"
-#include "P226.h"
-#include "P33.h"
-#include "P58.h"
-#include "P342.h"
-#include "P234.h"
-#include "P32.h"
-#include "P233.h"
-#include "P402.h"
-#include "P3.h"
-#include "P4.h"
-#include "P5.h"
-#include "P6.h"
-#include "P7.h"
-#include "P8.h"
-#include "P9.h"
-#include "P10.h"
-#include "P16.h"
-#include "P17.h"
-#include "P18.h"
-#include "P19.h"
-#include "P20.h"
-#include "P21.h"
-#include "P22.h"
-#include "P23.h"
-#include "P24.h"
-#include "P25.h"
-#include "P26.h"
-#include "P28.h"
-#include "P29.h"
-#include "P30.h"
-#include "P31.h"
-#include "P34.h"
-#include "P35.h"
-#include "P36.h"
-#include "P37.h"
-#include "P38.h"
-#include "P39.h"
-#include "P40.h"
-#include "P41.h"
-#include "P42.h"
-#include "P43.h"
-#include "P44.h"
-#include "P45.h"
-#include "P46.h"
-#include "P47.h"
-#include "P48.h"
-#include "P49.h"
-#include "P50.h"
+#include "P1~50\P1~50.h"
 
 int main()
 {
     cout << "LeetCode Practice" << endl;
-    SolveSudokuTest();
-    //FindSubstringTest();
-    //WildcardMatchingTest();
-    //TrapTest();
-    //JumpTest();
-    //GroupAnagramsTest();
-    //MyPowTest();
-    //RotageTest();
-    //CombinationSum2Test();
-    //CombinationSumTest();
-    //MultiplyTest();
-    //IsValidSudokuTest();
-    //SearchInsertTest();
-    //SearchRangeTest();
-    //NextPermutationTest();
-    //PermuteUniqueTest();
-    //CountAndSayTest();
-    //FirstMissingPositiveTest();
-    //PermuteTest();
-    //DivideTest();    //StrStrTest();
-    //RemoveDuplicatesTest();
-    //ReverseKGroupTest();
-    //SwapPairsTest();
-    //MergeKListsTest();
-    //GeneratParenthesisTest();
-    //MergeTwoListsTest();
-    //IsValidTest();
-    //RemoveNthFromEndTest();
-    //FourSumTest();
-    //LetterCombinationsTest();
-    //ThreeSumClosestTest();
-    //IsMatchTest();
-    //IsPalindromeNumTest();
-    //MyAtoiTest();
-    //ReverseTest();
-    //ConvertTest();
-    //LongestPalindromeTest();
-    //FindMedianSortedArrays();
-    //LengthOfLongestSubstringTest();
-    //RemoveKDigitsTest();
-    //CountDigitOneTest();
-    //LongestValidParenthesesTest();
-    //IsPalindromeTest();
-    //PowerOfFourTest();
-    //LengthOfLastWordTest();
-    //SearchTest();
-    //InvertBinaryTreeTest();
-    //CombinationTest();
-    //LongestCommonPrefixTest();
-    //IntToRomanTest();
-    //MaxAreaTest();
-    //Solution s;
-    //s.test();
+    P1_50();
 }
