fe2850c6e4c43058536a69899488f1886389705c
LeoBrilliant <LeoBrilliant@gmail.com>
2016-11-18 19:12:03 +0800

    调用DLL

--- main.cpp
+++ main.cpp
@@ -10,9 +10,14 @@
 //#include "P1~50\P1~50.h"
 #include "P51~100\P51~100.h"
 
+extern void P101_150();
+
 int main()
 {
     cout << "LeetCode Practice" << endl;
-    P51_100();
+    P101_150();
+    //P51_100();
     //P1_50();
+    Solutions s;
+    s.TestLastSolutions();
 }
