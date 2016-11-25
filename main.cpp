e1d946bb8b763ba151a7467f1795da6401f4e566
LeoBrilliant <LeoBrilliant@gmail.com>
2016-11-25 19:00:33 +0800

    可执行程序瘦身，程序库全部移至LeetCodeLib

--- main.cpp
+++ main.cpp
@@ -8,15 +8,16 @@
 #include "Common.h"
 
 //#include "P1~50\P1~50.h"
-#include "P51~100\P51~100.h"
+//#include "P51~100\P51~100.h"
 
 extern void P101_150();
+extern void P51_100();
 
 int main()
 {
     cout << "LeetCode Practice" << endl;
     P101_150();
-    //P51_100();
+    P51_100();
     //P1_50();
     Solutions s;
     s.TestLastSolutions();
